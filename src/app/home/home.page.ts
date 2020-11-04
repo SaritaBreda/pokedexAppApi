4d324f11bf05e808916292f29a3297f41a162f17
Sarita Breda <sarita.gabani@etec.sp.gov.br>
2020-11-04 12:35:31 -0300

    Correções Finais

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,45 +7,58 @@ import { UserService } from '../services/user.service';
   styleUrls: ['home.page.scss'],
 })
 
-/* Projeto realizado com auxílio da aluna Fernanda Pintucci */
-
 export class HomePage {
 
-  public listaPokemonExibir: any = [];
-  public totalPokemon: number;
+  public listaPokemon = [];
+  public listaPokemonExibir = [];
+
   public pagina = 1;
-  public totalPaginas = 105;
+  public totalPaginas = 0;
 
-  private url = 'https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0';
+  public next: string;
+  public previous: string;
 
-  constructor(private userService: UserService) { }
-
-  ionViewWillEnter() {
-    this.buscarPokemons(1);
+  constructor(private userService: UserService) {
+    this.buscarPokemon();
   }
 
-
-  public buscarPokemons(pagina: number) {
-    if (pagina <= 0) {
-      pagina = 1;
-    }
-
-    this.pagina = pagina;
-
-    this.userService.buscarPokemon(this.url).subscribe(dados => {
-      this.listaPokemonExibir = [];
-
-      this.totalPokemon = dados['count'];
+  public async buscarPokemon() {
+    await this.userService.buscarPokemon().subscribe(dados => {
+      this.listaPokemon = [];
+      this.totalPaginas = dados['count'] / 10;
+      this.previous = dados['previous'];
+      this.next = dados['next'];
 
       let listaApi = dados['results'];
 
       for (let pokemon of listaApi) {
         this.userService.buscarUmPokemon(pokemon.url).subscribe(dadosPokemon => {
-          this.listaPokemonExibir.push(dadosPokemon);
+          this.listaPokemon.push(dadosPokemon);
+
+          this.ordenarLista();
         });
-
       }
-
     });
+  }
+
+  private ordenarLista() {
+    this.listaPokemon.sort((a, b) => {
+      if (a.id > b.id) {
+        return 1;
+      }
+      if (a.id < b.id) {
+        return -1;
+      }
+        return 0;
+    });
+    this.listaPokemonExibir = this.listaPokemon;
+  }
+
+  public paginacao(url, movimento) {
+    this.pagina = this.pagina + movimento;
+
+    this.userService.url = url;
+
+    this.buscarPokemon();
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -6,31 +6,16 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
 
-  private url = 'https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0';
+  public url = 'https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0';
   
   constructor(private http: HttpClient) { }
 
-  /* Projeto realizado com auxílio da aluna Fernanda Pintucci */
-
-  public buscarTodos(pagina: number) {
-    if (pagina <= 0) {
-      pagina = 1;
-    }
-
-    return this.http.get(`${this.url}?page=${pagina}`);
-  }
-
-  public buscarPokemon(url: string) {
+  public buscarPokemon() {
     return this.http.get(this.url);
   }
 
   public buscarUmPokemon(url: string) {
-    return this.http.get(`${url}`);
-  }
-
-
-  public buscarId(id: number) {
-    return this.http.get(`${this.url}/${id}`);
+    return this.http.get(url);
   }
 
 }
